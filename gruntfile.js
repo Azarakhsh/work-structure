d17ea6446c1f820e3ef297491e0d435309e1152e
azarakhsh <azarakhsh.sh@gmail.com>
2015-09-13 09:48:21 +0430

    adds js/css vendors - adds cssmin plugin

--- gruntfile.js
+++ gruntfile.js
@@ -1,16 +1,34 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-compass');
 
 	grunt.initConfig({
 		uglify: {
-			my_target : {
+			custom_js : {
 				files:{
 					'assets/js/custom.js': ['develop/components/js/*.js']
 				} //files
-			} //my_target
-		}, //uglify
+			}, //custom-js
+			vendors_js : {
+				files: {
+					'assets/js/vendors/vendors.js': ['develop/components/js/vendors/*.js']
+				} //files
+			} // vendors_js
+		}, //uglify js
+		cssmin: {
+		  options: {
+		  	keepSpecialComments: 0,
+		    shorthandCompacting: false,
+		    roundingPrecision: -1
+		  }, //options
+		  target: {
+		    files: {
+		      'assets/css/vendors/vendors.css': ['develop/components/css/vendors/*.css']
+		    }
+		  } //target
+		}, //cssmin
 		compass: {
 			dev: {
 				options: {
@@ -20,10 +38,18 @@ module.exports = function(grunt) {
 		}, //compass
 		watch: {
 			options: { livereload: true },
-			scripts: {
-				files:['develop/components/js/*.js'],
-				tasks: ['uglify']
+			custom_js: {
+				files:['develop/components/js/custom.js'],
+				tasks: ['uglify:custom_js']
 			}, //script
+			vendors_js: {
+				files:['develop/components/js/vendors/*.js'],
+				tasks: ['uglify:vendors_js']
+			}, //vendors_js
+			vendors_css: {
+				files:['develop/components/css/vendors/*.css'],
+				tasks: ['cssmin']
+			}, //vendors_css
 			sass: {
 				files:['develop/components/sass/*.scss'],
 				tasks: ['compass:dev']
@@ -32,6 +58,6 @@ module.exports = function(grunt) {
 				files:['*.html']
 			} //html
 		} //watch
-	}) //initConfig
+	}); //initConfig
 	grunt.registerTask('default', 'watch');
 } //exports
